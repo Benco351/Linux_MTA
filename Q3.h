c87a7b7dc938b7397d87ec4980b7e5c3dcc2da21
Ben cohen <83662617+Benco351@users.noreply.github.com>
2023-04-18 13:17:06 +0300

    added #ifndef

--- Q3.h
+++ Q3.h
@@ -1,4 +1,7 @@
-#include "Utilities.h"
+#ifndef Q3_H
+#define Q3_H
+  #include "Utilities.h"
 
-void findAirCrafts(FILE* f, char** aircrafts, int nofAirCrafts);
-void printQ3(FlightData FD);
+  void findAirCrafts(FILE* f, char** aircrafts, int nofAirCrafts);
+  void printQ3(FlightData FD);
+#endif Q3_H
