069a88b2ebd6f7b88b4fee4f05079dd98e2d040d
EliorMigdal <104457430+EliorMigdal@users.noreply.github.com>
2023-06-07 20:30:40 +0300

    Update main.c

--- Assignment#2/main.c
+++ Assignment#2/main.c
@@ -3,10 +3,10 @@
 int main() {
     int pipeToChild[2]; // Pipe for sending numbers from parent to child
     int pipeToParent[2]; // Pipe for sending signals from child to parent
-    int number, result;
+    int number;
     pid_t pid;
     DB* dataBase;
-    bool first = true;
+    bool firstChildIteration = true, firstParentIteration = true;
 
     if (pipe(pipeToChild) == -1 || pipe(pipeToParent) == -1) {
         fprintf(stderr, "Pipe failed.\n");
@@ -25,6 +25,12 @@ int main() {
         signal(SIGINT, sigint_handler);
 
         while (true) {
+            int doesDBexist;
+            if (firstParentIteration)
+            {
+                read(pipeToParent[0], &doesDBexist, sizeof(int));
+                firstParentIteration = false;
+            }
             printMenu();
             scanf("%d", &number);
 
@@ -34,15 +40,29 @@ int main() {
                 continue;
             }
 
-            write(pipeToChild[1], &number, sizeof(number)); // Write the number to the pipe
+            if (number > 3)
+            {
+                write(pipeToChild[1], &number, sizeof(number)); // Write the number to the pipe
+            }
 
             if (number >= 1 && number <= 3)
             {
-                int inputArrSize = 0, outputArrSize = 0, currentStringSize = 0;
+                int inputArrSize = 0, outputArrSize = 0;
                 char** output = NULL, **input = NULL;
-                input = readInput(&inputArrSize);
-                write(pipeToChild[1], &inputArrSize, sizeof(int));
+                input = readInput(&inputArrSize, number);
 
+                if (doesDBexist == DB_DOES_NOT_EXISTS)
+                {
+                    int generate = GENERATE_DB;
+                    printf("Generating Database....\n");
+                    write(pipeToChild[1], &generate, sizeof(generate));
+                    write(pipeToChild[1], &inputArrSize, sizeof(int));
+                    ReadOrWriteToPipe(input, inputArrSize, pipeToChild, WRITE);
+                    read(pipeToParent[0], &doesDBexist, sizeof(int));
+                }
+
+                write(pipeToChild[1], &number, sizeof(number));
+                write(pipeToChild[1], &inputArrSize, sizeof(int));
                 ReadOrWriteToPipe(input, inputArrSize, pipeToChild, WRITE);
                 read(pipeToParent[0], &outputArrSize, sizeof(int));
                 output = (char**)malloc(sizeof(char*) * outputArrSize);
@@ -54,20 +74,42 @@ int main() {
                     printf("%s", output[i]);
                     free(output[i]);
                 }
-
                 free(output);
 
                 for (int i = 0; i < inputArrSize; i++)
                 {
                     free(input[i]);
                 }
-
                 free(input);
 
             }
 
-            if (number == 7)
+            else if (number == 4)
+            {
+                printf("Successfully updated the DB.\n");
+            }
+
+            else if (number == 5)
+            {
+                printf("Successfully zipped the DB files.\n");
+            }
+
+            else if (number == 6)
+            {
+                pid_t childPID;
+                read(pipeToParent[0], &childPID, sizeof(pid_t));
+                printf("Child process PID: %d\n", childPID);
+            }
+
+            else if (number == 7)
+            {
+                int exitCode;
+                read(pipeToParent[0], &exitCode, sizeof(pid_t));
+                printf("Gracefully exiting.\nChild process's exit code: %d\n", exitCode);
                 exit(EXIT_SUCCESS);
+            }
+
+            printf("\n");
         }
 
         close(pipeToChild[1]); // Close the write end of the pipe for numbers
@@ -76,37 +118,40 @@ int main() {
         wait(NULL); // Wait for the child process to finish
     } else if (pid == 0) {
         // Child process
-        printf("1\n");
-        if (first)
+        if (firstChildIteration)
         {
-            printf("2\n");
-            //check if zip file exists; if it does, unzip it. if it doesn't, move on.
+            bool zipExists = doesZipExists();
+            int checkResult = 0;
 
-            if (true) //if file exists
+            if (zipExists)
             {
-                printf("3\n");
                 //unzip
                 char** dirList = NULL;
                 int listSize = 0;
                 dirList = createDirList(&listSize);
                 dataBase = getDataBase(listSize, dirList);
-                printf("Im here?\n");
+                checkResult = DB_EXISTS;
             }
 
-            first = false;
+            else if (!zipExists)
+            {
+                checkResult = DB_DOES_NOT_EXISTS;
+            }
+
+            firstChildIteration = false;
+            write(pipeToParent[1], &checkResult, sizeof(checkResult));
         }
 
-        while (true) {
-            printf("Or am I here!\n");
-            read(pipeToChild[0], &number, sizeof(number)); // Read the number from the pipe
-            printf("Did I read the pipe?\n");
+        while (true)
+        {
+            read(pipeToChild[0], &number, sizeof(number));
             child_process(pipeToChild, pipeToParent, number, dataBase);
         }
 
-        close(pipeToChild[0]); // Close the read end of the pipe for numbers
-        close(pipeToParent[1]); // Close the write end of the pipe for signals
+        close(pipeToChild[0]);
+        close(pipeToParent[1]);
 
-        exit(0); // Terminate the child process
+        exit(0);
     }
 
     return 0;
